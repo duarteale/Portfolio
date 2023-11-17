06dfd2737b1d6c5a520a476f0aba35f83ac505c1
AleDuarte <duartealejandrame@gmail.com>
2023-11-16 23:14:36 -0300

    edicion del navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,16 @@
-// Selecciona el body del documento
 const body = document.body;
-
-// Variable para rastrear el modo (inicialmente falso)
-let modoOscuro = false;
+const modoSwitch = document.getElementById('modoSwitch');
 
 // Función para cambiar el modo de visualización del sitio
 function cambiarModo() {
-      modoOscuro = !modoOscuro; //----------------------------- Invierte el estado del modo
-      body.classList.toggle("modo-oscuro"); //----------------- Agrega o quita la clase "modo-oscuro" al body y se configura (aparte) en el archivo styles.css
-      actualizarTextoBoton(); //----------------------------- Llama a la función para actualizar el texto del botón
-}
-// Busco el botón y agrego un "escuchador de eventos" para darle comportamiento al clic
-const botonModoNavbar = document.getElementById('botonModo');
-botonModoNavbar.addEventListener("click", cambiarModo);
-
-//Funcion que cambia el texto del boton
-function actualizarTextoBoton() {
-      if (modoOscuro) {
-            botonModo.innerText = "Día"; //----------------------------- Al hacer clic en "Noche" de pone en Negro el fondo y el boton dice "Dia"
+      if (modoSwitch.checked) {
+            body.classList.add("modo-oscuro");
       } else {
-            botonModo.innerText = "Noche"; //----------------------------- Al hacer clic en "Día" de pone el fondo de colores  y el boton dice "Noche"
+            body.classList.remove("modo-oscuro");
       }
 }
-// Configuro estilos para el botón
-botonModo.style.top = '55px'; 
-botonModo.style.right = '20px'; 
-botonModo.style.zIndex = '1000'; 
-botonModo.style.borderRadius = '10px'; 
-actualizarTextoBoton();
+// Agregar evento al cambio del switch para activar el modo oscuro
+modoSwitch.addEventListener('change', cambiarModo);
 //---------------- Fin Modo oscuro/claro ------------------
 
 
